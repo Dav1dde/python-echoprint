a5390f282c2675e557b6431fa4bebfcbeaa09842
Dav1dde <admin@dav1d.de>
2015-06-09 01:20:24 +0200

    improve c extension to support any kind of sequence (iterator)

--- echoprint.cpp
+++ echoprint.cpp
@@ -15,23 +15,22 @@ static PyObject * echoprint_codegen(PyObject *self, PyObject *args) {
     if (!PyArg_ParseTuple(args, "O|i", &py_samples, &start_offset)) {
         return NULL;
     }
-    if (!PySequence_Check(py_samples)) {
-        PyErr_SetString(PyExc_TypeError, "expected sequence");
-        return NULL;
-    }
-    num_samples = PySequence_Size(py_samples);
+
+    py_samples = PySequence_Fast(py_samples, "expected a sequence");
+    num_samples = PySequence_Length(py_samples);
     samples = new float[num_samples];
 
     for (i = 0; i < num_samples; i++) {
-        item = PySequence_GetItem(py_samples, i);
+        item = PySequence_Fast_GET_ITEM(py_samples, i);
         if (!PyFloat_Check(item)) {
             delete[] samples;
             PyErr_SetString(PyExc_TypeError, "expected sequence of floats");
             return NULL;
         }
         samples[i] = (float)PyFloat_AsDouble(item);
-        Py_DECREF(item);
     }
+    Py_DECREF(py_samples);
+
     pCodegen = new Codegen(samples, num_samples, start_offset);
     result = Py_BuildValue("{s:s,s:f}",
         "code", pCodegen->getCodeString().c_str(),
@@ -44,7 +43,10 @@ static PyObject * echoprint_codegen(PyObject *self, PyObject *args) {
 
 static PyMethodDef echoprint_methods[] = {
     {"codegen", echoprint_codegen, METH_VARARGS,
-     "Generates a echoprint code for a list of floating point PCM data sampled at 11025 Hz and mono. Optionally takes a second integer argument to hint the server on where the sample is taken from in the original file if known."},
+     "Generates a echoprint code for a list of floating point PCM data sampled at 11025 Hz and mono. "
+     "Optionally takes a second integer argument to hint the server "
+     "on where the sample is taken from in the original file if known."
+    },
     {NULL, NULL, 0, NULL}
 };
 
