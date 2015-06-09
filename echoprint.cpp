bccc5191fd3692bfa425d04fe4cc4ff989fd2b47
Dav1dde <admin@dav1d.de>
2015-06-09 15:23:36 +0200

    support any iterable

--- echoprint.cpp
+++ echoprint.cpp
@@ -6,9 +6,9 @@ static PyObject * echoprint_codegen(PyObject *self, PyObject *args) {
     PyObject *py_samples;
     int start_offset = 0;
     PyObject *item;
-    float *samples;
-    uint num_samples;
-    uint i;
+    //float *samples;
+    std::vector<float> samples;
+    unsigned int num_samples;
     Codegen *pCodegen;
     PyObject *result;
     
@@ -16,34 +16,39 @@ static PyObject * echoprint_codegen(PyObject *self, PyObject *args) {
         return NULL;
     }
 
-    py_samples = PySequence_Fast(py_samples, "expected a sequence");
-    num_samples = PySequence_Length(py_samples);
-    // prevent segmentation faults in the codegen
-    if (num_samples == 0) {
-        Py_DECREF(py_samples);
-
-        Py_RETURN_NONE;
+    if (PyObject_HasAttrString(py_samples, "__len__")) {
+        samples.reserve(PyObject_Size(py_samples));
     }
-    samples = new float[num_samples];
 
-    for (i = 0; i < num_samples; i++) {
-        item = PySequence_Fast_GET_ITEM(py_samples, i);
+    py_samples = PyObject_GetIter(py_samples);
+    if (py_samples == NULL) {
+        PyErr_SetString(PyExc_TypeError, "samples is not iterable");
+        return NULL;
+    }
+
+    for (num_samples = 0; item = PyIter_Next(py_samples); ++num_samples) {
         if (!PyFloat_Check(item)) {
-            delete[] samples;
+            Py_DECREF(item);
             PyErr_SetString(PyExc_TypeError, "expected sequence of floats");
             return NULL;
         }
-        samples[i] = (float)PyFloat_AsDouble(item);
+
+        samples.push_back((float)PyFloat_AsDouble(item));
+        Py_DECREF(item);
     }
     Py_DECREF(py_samples);
 
-    pCodegen = new Codegen(samples, num_samples, start_offset);
+    // prevent segfault in codegen, also &samples.front()
+    if (num_samples == 0) {
+        Py_RETURN_NONE;
+    }
+
+    pCodegen = new Codegen(&samples.front(), num_samples, start_offset);
     result = Py_BuildValue("{s:s,s:f}",
         "code", pCodegen->getCodeString().c_str(),
         "version", pCodegen->getVersion()
     );
     delete pCodegen;
-    delete[] samples;
     return result;
 }
 
